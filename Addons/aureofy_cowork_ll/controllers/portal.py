843e6484c18705fbd7dc977c02440286df20b4c3
Lucas <llabeau@aureofy.com>
2026-01-06 15:05:39 -0300

    v1.2.4

--- Addons/aureofy_cowork_ll/controllers/portal.py
+++ Addons/aureofy_cowork_ll/controllers/portal.py
@@ -2,111 +2,32 @@
 
 from odoo import http, _
 from odoo.http import request
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
+class CoworkPortal(CustomerPortal):
 
-class CoworkPortalController(CustomerPortal):
+    @http.route(['/my/credits/buy'], type='http', auth="user", website=True)
+    def portal_buy_credits(self, **kwargs):
+        values = self._prepare_portal_layout_values()
+        return request.render("aureofy_cowork_ll.portal_buy_credits", values)
 
-    def _prepare_home_portal_values(self, counters):
-        values = super()._prepare_home_portal_values(counters)
+    @http.route(['/my/credits/buy/submit'], type='http', auth="user", website=True, methods=['POST'])
+    def portal_buy_credits_submit(self, **kwargs):
+        amount = int(kwargs.get('amount', 0))
+        if amount <= 0:
+            return request.redirect('/my/credits/buy')
+            
         partner = request.env.user.partner_id
+        product = request.env.ref('aureofy_cowork_ll.product_credits')
         
-        if 'membership_count' in counters:
-            values['membership_count'] = request.env['cowork.membership'].search_count([
-                ('partner_id', '=', partner.id)
-            ])
+        # Create Sale Order
+        so = request.env['sale.order'].create({
+            'partner_id': partner.id,
+            'order_line': [(0, 0, {
+                'product_id': product.id,
+                'product_uom_qty': amount,
+            })]
+        })
         
-        return values
-
-    @http.route(['/my/memberships', '/my/memberships/page/<int:page>'], 
-                type='http', auth='user', website=True)
-    def portal_memberships(self, page=1, sortby=None, filterby=None, **kw):
-        """Lista de membresías del usuario"""
-        partner = request.env.user.partner_id
-        Membership = request.env['cowork.membership']
-        
-        domain = [('partner_id', '=', partner.id)]
-        
-        # Sorting
-        searchbar_sortings = {
-            'date': {'label': _('Fecha'), 'order': 'date_start desc'},
-            'name': {'label': _('Número'), 'order': 'name'},
-            'state': {'label': _('Estado'), 'order': 'state'},
-        }
-        if not sortby:
-            sortby = 'date'
-        order = searchbar_sortings[sortby]['order']
-        
-        # Filters
-        searchbar_filters = {
-            'all': {'label': _('Todas'), 'domain': []},
-            'active': {'label': _('Activas'), 'domain': [('state', '=', 'active')]},
-            'expired': {'label': _('Expiradas'), 'domain': [('state', '=', 'expired')]},
-        }
-        if not filterby:
-            filterby = 'all'
-        domain += searchbar_filters[filterby]['domain']
-        
-        # Count
-        membership_count = Membership.search_count(domain)
-        
-        # Pager
-        pager = portal_pager(
-            url='/my/memberships',
-            url_args={'sortby': sortby, 'filterby': filterby},
-            total=membership_count,
-            page=page,
-            step=10,
-        )
-        
-        # Memberships
-        memberships = Membership.search(domain, order=order, limit=10, offset=pager['offset'])
-        
-        values = {
-            'memberships': memberships,
-            'page_name': 'memberships',
-            'pager': pager,
-            'default_url': '/my/memberships',
-            'searchbar_sortings': searchbar_sortings,
-            'sortby': sortby,
-            'searchbar_filters': searchbar_filters,
-            'filterby': filterby,
-        }
-        
-        return request.render('aureofy_cowork_ll.portal_my_memberships', values)
-
-    @http.route(['/my/membership/<int:membership_id>'], type='http', auth='user', website=True)
-    def portal_membership_detail(self, membership_id, **kw):
-        """Detalle de una membresía"""
-        partner = request.env.user.partner_id
-        membership = request.env['cowork.membership'].browse(membership_id)
-        
-        if not membership.exists() or membership.partner_id != partner:
-            return request.redirect('/my/memberships')
-        
-        values = {
-            'membership': membership,
-            'page_name': 'membership_detail',
-        }
-        
-        return request.render('aureofy_cowork_ll.portal_membership_detail', values)
-
-    @http.route(['/my/membership/<int:membership_id>/download'], type='http', auth='user', website=True)
-    def portal_membership_download(self, membership_id, **kw):
-        """Descargar PDF de membresía"""
-        partner = request.env.user.partner_id
-        membership = request.env['cowork.membership'].browse(membership_id)
-        
-        if not membership.exists() or membership.partner_id != partner:
-            return request.redirect('/my/memberships')
-        
-        pdf_content, _ = request.env['ir.actions.report'].sudo()._render_qweb_pdf(
-            'aureofy_cowork_ll.action_report_membership', [membership.id]
-        )
-        
-        headers = [
-            ('Content-Type', 'application/pdf'),
-            ('Content-Disposition', 'attachment; filename="Membresia-%s.pdf"' % membership.name),
-        ]
-        
-        return request.make_response(pdf_content, headers=headers)
+        # Redirect to the Sale Order for payment
+        return request.redirect(so.get_portal_url())
